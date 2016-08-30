9ee5019c8afb76cd48ab4f7bbf2f52543ee7895e
JorikJ <jjeening@avans.nl>
2016-08-30 11:08:35 +0200

    commit

---
+++ ConsoleApplication1/ConsoleApplication1/AgeCalc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class AgeCalc
+    {
+        private String input;
+        private DateTime birthdate;
+        public AgeCalc(String input)
+        {
+            this.input = input;
+            
+        }
+
+        // checks if input is date
+        public Boolean check()
+        {
+            Boolean date = false;
+
+            try
+            {
+                birthdate = DateTime.Parse(input);
+                date = true;
+
+                calculateAge();
+
+            }
+            catch { 
+            date =  false;
+                Console.WriteLine("invalid date");
+        }
+            return date;
+        }
+
+        public void calculateAge()
+        {
+            DateTime dt = DateTime.Today;
+
+            int age = (int) dt.Subtract(birthdate).TotalDays/365;
+
+            TimeSpan timespan = dt.Subtract(birthdate);
+
+
+
+            Console.WriteLine(age);
+
+            //Console.WriteLine(dt.Subtract(birthdate)+"is the age");
+            Console.ReadLine();
+            
+
+              
+
+        }
+
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            Console.WriteLine("type a valid date to calculate the age");
+            input = Console.ReadLine();
+
+            AgeCalc calc = new AgeCalc(input);
+
+            calc.check();
+            
+            
+
+        }
+    }
+}
